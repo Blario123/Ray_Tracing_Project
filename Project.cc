6bb111f384f246783ca7bcd1fca757a0c2003d12
Rupinder <rupinder.matharu@student.manchester.ac.uk>
2021-10-03 17:22:42 +0100

    Added to the Project.cc code and renamed a file. Renamed header_tests.cpp to header_tests.cc for consistency. Added #include for Image.h and Vec3.h, added a pure virtual function to the Base class for physical objects representing the light emitted. Added radius and centre member data to the Sphere object.

--- Project.cc
+++ Project.cc
@@ -1,17 +1,42 @@
+// Define basic input/output stream objects
 #include <iostream>
 
+// Include image write implementation
+#include "Image.h"
+
+// Include 3D vectors (Already included in Image.h but kept here for
+// readability)
+#include "Vec3.h"
+
+// The base class for physical objects present in the scene
 class PhysicalObject
 {
 public:
-private:
+  double virtual Light_Emitted(Vec3 &position_vector,
+                               Vec3 &output_direction) = 0;
 };
 
+// The derived class of Sphere representing spheres in the scene
 class Sphere : public PhysicalObject
 {
-private:
+public:
+  Sphere(const Vec3 &Centre_Input, const double &Radius_Input)
+  {
+    centre = Centre_Input;
+    radius = Radius_Input;
+  }
+
+  Vec3 centre;
+  double radius;
+  double Light_Emitted(Vec3 &position_vector, Vec3 &output_direction)
+  {
+    return 0.0;
+  }
 };
 
 int main()
 {
-  std::cout << "Hello World" << std::endl;
+  Vec3 vector(1.0, 1.0, 1.0);
+  Sphere sphere1(vector, 1.0);
+  std::cout << sphere1.Light_Emitted(vector, vector) << std::endl;
 }
